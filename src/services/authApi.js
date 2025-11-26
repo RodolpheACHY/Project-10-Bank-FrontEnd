776410b7a05835459435281128ee9823830a2d1e
Rodolphe ACHY <125242522+RodolpheACHY@users.noreply.github.com>
2025-11-27 00:12:27 +0100

    Use environment variable for API base URL

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const authApi = createApi({
     reducerPath: 'authApi',
     baseQuery: fetchBaseQuery({
-      baseUrl: 'http://localhost:3001/api/v1/',
+      baseUrl: import.meta.env.VITE_API_URL,
       // Prépare l'en-tête d'autorisation pour toutes les requêtes si un token est présent
       prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token; // Accédez au token depuis le store Redux
